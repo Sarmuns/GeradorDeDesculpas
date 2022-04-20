806919c331424f5c3a686325c799d710cfa2c407
Samuel Rocha <samuel.afrocha@gmail.com>
2022-04-19 22:17:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var intro = [`soRry i can't come`,`pleaSe forgive my absence`]
-var scapeGoat = [`my nephEw`,`the ghost of hitler`]
-var delay = [`just shit the bed`,`died in front of me`]
+var intro = ["sorry i can't come",'please forgive my abscence','this is going to sound crazy but','get this:',"i can't go because","i know you're going to hate me but","i was minding my own business and boom!","i feel terrible but","i regretfully cannot attend,","this is going to sound like an excuse but"]
+var scapeGoat = [`my nephEw`,`the ghost of hitler`,"the Pope", "my ex", "a high school marching band", "Elton John", "a sad clown", "the kid from Air Bud", "a professional cricket team","my tinder date"]
+var delay = [`just shit the bed`,`died in front of me`,"won't stop telling me knock knock jokes","is having a nervous breakdown","gave me syphilis","poured lemonade in my gas tank","stabbed me","found my box of human teeth","stole my bike","posted my nudes on instagram"]
 
 
 function normalizeText(string){
